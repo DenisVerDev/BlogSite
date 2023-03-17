2a9c70453b4196b2e91b995ec99f1862561804bd
DenisVerDev <denisverepadev@gmail.com>
2023-03-17 17:42:22 +0200

    updated components links and js

--- BlogSite/wwwroot/js/posts_filter.js
+++ BlogSite/wwwroot/js/posts_filter.js
@@ -1,22 +1,11 @@
 ﻿$(document).ready(function () {
 
-    $('#filter-themes').change(function () {
+    function submitFilter() {
         $('#filter-page-num').val(1);
         $('#filter-form').submit();
-    });
+    }
 
-    $('#filter-periods').change(function () {
-        $('#filter-page-num').val(1);
-        $('#filter-form').submit();
-    });
-
-    $('#filter-most-popular').change(function () {
-        $('#filter-page-num').val(1);
-        $('#filter-form').submit();
-    });
-
-    $('#filter-only-favorites').change(function () {
-        $('#filter-page-num').val(1);
-        $('#filter-form').submit();
+    $('.filter-input').change(function () {
+        submitFilter();
     });
 });
